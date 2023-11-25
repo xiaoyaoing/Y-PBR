a0beec916e06afea6333acdb51845fa74b9dab0d
xiaoyaoing <1924188282@qq.com>
2023-11-25 15:45:59 +0800

    save

---
+++ src/Bsdfs/ThinSheet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pc on 2023/10/30.
+//
+
+#include "ThinSheet.h"
---
+++ src/Bsdfs/ThinSheet.h
@@ -0,0 +1,16 @@
+//
+// Created by pc on 2023/10/30.
+//
+
+#ifndef Y_PBR_THINSHEET_H
+#define Y_PBR_THINSHEET_H
+
+
+
+class ThinSheet {
+
+};
+
+
+
+#endif //Y_PBR_THINSHEET_H
